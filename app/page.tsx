da77311148cd96819412ea65956481931e36061a
pm <liberatheon@gmail.com>
2025-06-03 01:59:39 +0700

    Add pnpm lockfile and enhance global styles
    - Created a new pnpm-lock.yaml file to manage package dependencies.
    - Added keyframe animations for background position and a gradient border style in globals.css.
    - Updated the Hero component to utilize motion effects and improved layout with a gradient border.
    - Introduced a new NeonGradientCard component for enhanced UI with customizable neon gradient effects.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center min-h-screen bg-black px-5'>
+    <div className='flex justify-center items-center min-h-screen bg-black px-5 pb-10'>
       <Hero />
     </div>
   );
