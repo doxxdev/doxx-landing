5297a4542cc48c71136fa875ff2c5660f0f5b239
doxxdev <Doxx.developer@proton.me>
2025-06-09 00:00:38 +0700

    Update Navbar component to center content and limit width for better layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Navbar() {
   return (
-    <nav className='h-[72px] flex items-center justify-between px-6'>
+    <nav className='h-[72px] flex items-center justify-between px-6 max-w-[1480px] mx-auto'>
       <Image src='/doxx-icon.svg' alt='Doxx' width={124} height={36} priority />
       <span className='text-sm font-medium text-primary   '>[Coming Soon]</span>
     </nav>
